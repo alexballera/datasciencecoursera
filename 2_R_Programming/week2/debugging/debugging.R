7fe02597c6b3d247cf1e2d2ef2764a47e88109bb
Alex Ballera <alexballera@gmail.com>
2020-06-03 13:22:40 -0300

    debugging

---
+++ 2_R_Programming/week2/debugging/debugging.R
@@ -0,0 +1,14 @@
+# traceback: imprime la función que tiene el error, el error más reciente
+
+mean(x)
+traceback()
+
+# debug: abre el navegador y depura la función línea por línea
+
+debug(lm)
+lm(x - y)
+
+# recover: nos muestra opciones en las diferentes funciones para depurar
+
+options(error = recover)
+read.csv("nohayarchivo")
