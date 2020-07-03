5ed25d15f57a25e37b2c4527bf64d094e222ef77
Alex Ballera <alexballera@gmail.com>
2020-07-03 11:52:35 -0300

    Leyendo desde Mysql

---
+++ 3_Getting_and_Cleaning-Data/mysql.R
@@ -0,0 +1,23 @@
+# Conectando y listando las bases de datos
+## Conexión
+
+ucscDb <- dbConnect(MySQL(), user="genome", host="genome-mysql.cse.ucsc.edu")
+result <- dbGetQuery(ucscDb, "show databases;"); dbDisconnect(ucscDb);
+result
+
+hg19 <- dbConnect(MySQL(), user="genome", db="hg19", host="genome-mysql.cse.ucsc.edu")
+allTables <- dbListTables(hg19)
+length(allTables)
+allTables[1:5]
+dbListFields(hg19, "affyU133Plus2")
+dbGetQuery(hg19, "select count(*) from affyU133Plus2")
+# Read from the table
+affyData <- dbReadTable(hg19, "affyU133Plus2")
+head(affyData)
+# Select a specific subset
+query <- dbSendQuery(hg19, "select * from affyU133Plus2 where misMatches between 1 and 3")
+affyMis <- fetch(query); quantile(affyMis$misMatches)
+affyMisSmall <- fetch(query, n=10); dbClearResult(query)
+dim(affyMisSmall)
+dbDisconnect(hg19)
+
